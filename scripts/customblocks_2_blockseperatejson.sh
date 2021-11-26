d17e7bae7bf91af09e4a188edeca93477cee9c71
TheCrafterDays <colbydray@mail.com>
2021-11-26 03:22:18 -0500

    Update customblocks_2_blockseperatejson.sh

--- scripts/customblocks_2_blockseperatejson.sh
+++ scripts/customblocks_2_blockseperatejson.sh
@@ -5,11 +5,9 @@ LineTotal=$(expr $LineTotal)
 mkdir extracted_custom/blocks
 while [ $LineNum -le $LineTotal ]; 
 do 
-	cat extracted_custom/BlockNames.json | head -n $LineNum | tail -n 1
 	BlockName=$(cat extracted_custom/BlockNames.json | head -n $LineNum | tail -n 1)
-	BlockNameFile=$(echo $BlockName | sed 's/"//g' )
-	cat extracted_custom/BlockDefinitions.json | jq 'select(.name=='\"$BlockNameFile\"') | .value ' > extracted_custom/blocks/$BlockNameFile.json
-	echo ClassiCube2Minetest: Block: 2: Create Seperate Block JSON: $BlockNameFile
+	cat extracted_custom/BlockDefinitions.json | jq '.'\"$BlockName\"'' > extracted_custom/blocks/$BlockName.json
+	echo ClassiCube2Minetest: Block: Create Seperate Block JSON: $BlockName
 	LineNum=$(($LineNum+1))
 done
 
