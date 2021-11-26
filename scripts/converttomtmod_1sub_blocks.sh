a90abc70f9f79c89614f0c933ea0239f55d837a3
TheCrafterDays <colbydray@mail.com>
2021-11-26 03:23:14 -0500

    Update converttomtmod_1sub_blocks.sh

--- scripts/converttomtmod_1sub_blocks.sh
+++ scripts/converttomtmod_1sub_blocks.sh
@@ -8,37 +8,39 @@ function signedtounsigned (
 	echo $output
 )
 
-ID=$(cat $@ | jq '.[] | select(.name == "ID2") | .value')
-CollideType=$(cat $@ | jq '.[] | select(.name == "CollideType") | .value')
-Speed=$(cat $@ | jq '.[] | select(.name == "Speed") | .value')
-TextureNum1_signed=$(cat $@ | jq '.[] | select(.name == "Textures") | .value[0]')
-TextureNum2_signed=$(cat $@ | jq '.[] | select(.name == "Textures") | .value[1]')
-TextureNum3_signed=$(cat $@ | jq '.[] | select(.name == "Textures") | .value[2]')
-TextureNum4_signed=$(cat $@ | jq '.[] | select(.name == "Textures") | .value[3]')
-TextureNum5_signed=$(cat $@ | jq '.[] | select(.name == "Textures") | .value[4]')
-TextureNum6_signed=$(cat $@ | jq '.[] | select(.name == "Textures") | .value[5]')
-Textures256_1_signed=$(cat $@ | jq '.[] | select(.name == "Textures") | .value[6]')
-Textures256_2_signed=$(cat $@ | jq '.[] | select(.name == "Textures") | .value[7]')
-Textures256_3_signed=$(cat $@ | jq '.[] | select(.name == "Textures") | .value[8]')
-Textures256_4_signed=$(cat $@ | jq '.[] | select(.name == "Textures") | .value[9]')
-Textures256_5_signed=$(cat $@ | jq '.[] | select(.name == "Textures") | .value[10]')
-Textures256_6_signed=$(cat $@ | jq '.[] | select(.name == "Textures") | .value[11]')
-TransmitsLight=$(cat $@ | jq '.[] | select(.name == "TransmitsLight") | .value')
-WalkSound=$(cat $@ | jq '.[] | select(.name == "WalkSound") | .value')
-FullBright=$(cat $@ | jq '.[] | select(.name == "FullBright") | .value')
-Shape=$(cat $@ | jq '.[] | select(.name == "Shape") | .value')
-BlockDraw=$(cat $@ | jq '.[] | select(.name == "BlockDraw") | .value')
-FogR_signed=$(cat $@ | jq '.[] | select(.name == "Fog") | .value[0]')
-FogG_signed=$(cat $@ | jq '.[] | select(.name == "Fog") | .value[1]')
-FogB_signed=$(cat $@ | jq '.[] | select(.name == "Fog") | .value[2]')
-#Coords=$(cat $@ | jq '.[] | select(.name == "Coords") | .value')
-Coords1=$(cat $@ | jq '.[] | select(.name == "Coords") | .value[0]')
-Coords2=$(cat $@ | jq '.[] | select(.name == "Coords") | .value[1]')
-Coords3=$(cat $@ | jq '.[] | select(.name == "Coords") | .value[2]')
-Coords4=$(cat $@ | jq '.[] | select(.name == "Coords") | .value[3]')
-Coords5=$(cat $@ | jq '.[] | select(.name == "Coords") | .value[4]')
-Coords6=$(cat $@ | jq '.[] | select(.name == "Coords") | .value[5]')
-NameP=$(cat $@ | jq '.[] | select(.name == "Name") | .value')
+cat extracted_custom/blocks/Block00A4.json | jq '.ID2'
+
+ID=$(cat $@ | jq '.ID2')
+CollideType=$(cat $@ | jq '.CollideType')
+Speed=$(cat $@ | jq '.Speed')
+TextureNum1_signed=$(cat $@ | jq '.Textures[0]')
+TextureNum2_signed=$(cat $@ | jq '.Textures[1]')
+TextureNum3_signed=$(cat $@ | jq '.Textures[2]')
+TextureNum4_signed=$(cat $@ | jq '.Textures[3]')
+TextureNum5_signed=$(cat $@ | jq '.Textures[4]')
+TextureNum6_signed=$(cat $@ | jq '.Textures[5]')
+Textures256_1_signed=$(cat $@ | jq '.Textures[6]')
+Textures256_2_signed=$(cat $@ | jq '.Textures[7]')
+Textures256_3_signed=$(cat $@ | jq '.Textures[8]')
+Textures256_4_signed=$(cat $@ | jq '.Textures[9]')
+Textures256_5_signed=$(cat $@ | jq '.Textures[10]')
+Textures256_6_signed=$(cat $@ | jq '.Textures[11]')
+TransmitsLight=$(cat $@ | jq '.TransmitsLight')
+WalkSound=$(cat $@ | jq '.WalkSound')
+FullBright=$(cat $@ | jq '.FullBright')
+Shape=$(cat $@ | jq '.Shape')
+BlockDraw=$(cat $@ | jq '.BlockDraw')
+FogR_signed=$(cat $@ | jq '.Fog[0]')
+FogG_signed=$(cat $@ | jq '.Fog[1]')
+FogB_signed=$(cat $@ | jq '.Fog[2]')
+#Coords=$(cat $@ | jq '.Coords')
+Coords1=$(cat $@ | jq '.Coords[0]')
+Coords2=$(cat $@ | jq '.Coords[1]')
+Coords3=$(cat $@ | jq '.Coords[2]')
+Coords4=$(cat $@ | jq '.Coords[3]')
+Coords5=$(cat $@ | jq '.Coords[4]')
+Coords6=$(cat $@ | jq '.Coords[5]')
+NameP=$(cat $@ | jq '.Name')
 
 BlockName=$(echo $NameP | sed 's/"//g') 
 TextureNum1=$(signedtounsigned $TextureNum1_signed)
@@ -67,35 +69,43 @@ TextureNum6=$(expr $Textures256_6 \* 256 + $TextureNum6)
 CoordsString=$(python3 scripts/converttomtmod_1sub_blocksize.py $Coords1 $Coords2 $Coords3 $Coords4 $Coords5 $Coords6)
 
 echo 'minetest.register_node("classicubeconverted:'$ID'", {'
-echo '	inventory_image = minetest.inventorycube("texture.png"),'
-if [ "$CollideType" = 0 ] || [ "$CollideType" = 1 ] || [ "$CollideType" = 2 ] || [ "$CollideType" = 3 ];
-	then
-	echo '	drawtype = "nodebox",'
+echo '	description =  "'$BlockName'",'
+echo '	inventory_image = minetest.inventorycube("'$TextureNum1'.png"),'
+
+if [ "$Shape" != 0 ]; then
+	if [ "$CollideType" = 0 ] || [ "$CollideType" = 1 ] || [ "$CollideType" = 2 ] || [ "$CollideType" = 3 ];
+		then
+		echo '	drawtype = "nodebox",'
+		fi
+	if [ "$CollideType" = 4 ] || [ "$CollideType" = 5 ];
+		then
+		echo '	drawtype = "liquid",'
+		fi
+	else
+		echo '	drawtype = "plantlike",'
+	fi	
+
+if [ "$Shape" != 0 ]; then
+		echo '	tiles = { "'$TextureNum1'.png", "'$TextureNum2'.png", "'$TextureNum3'.png", "'$TextureNum4'.png", "'$TextureNum6'.png", "'$TextureNum5'.png" },'
+	else
+		echo '	tiles = { "'$TextureNum1'.png" },'
 	fi
-if [ "$CollideType" = 4 ] || [ "$CollideType" = 5 ];
-	then
-	echo '	drawtype = "liquid",'
-	fi
-echo '	tiles = { "'$TextureNum1'.png", "'$TextureNum2'.png", "'$TextureNum3'.png", "'$TextureNum4'.png", "'$TextureNum6'.png", "'$TextureNum5'.png" },'
-echo '	alpha = 255,'
+
 echo '	paramtype = "light",'
 
-if [ "$CollideType" = 1 ] || [ "$CollideType" = 2 ] || [ "$CollideType" = 3 ];
-	then
+if [ "$CollideType" = 2 ] || [ "$CollideType" = 3 ]; then
 	echo '	walkable = true,'
 	echo '	pointable = true,'
 	echo '	diggable = true,'
 	echo '	buildable_to = false,'
 	fi
-if [ "$CollideType" = 0 ] || [ "$CollideType" = 4 ] || [ "$CollideType" = 5 ];
-	then
+if [ "$CollideType" = 0 ] || [ "$CollideType" = 4 ] || [ "$CollideType" = 5 ]; then
 	echo '	walkable = false,'
 	echo '	pointable = false,'
 	echo '	diggable = false,'
 	echo '	buildable_to = true,'
 	fi
-if [ "$CollideType" = 1 ];
-	then
+if [ "$CollideType" = 1 ]; then
 	echo '	walkable = false,'
 	echo '	pointable = true,'
 	echo '	diggable = true,'
@@ -103,11 +113,48 @@ if [ "$CollideType" = 1 ];
 	fi
 
 echo '	is_ground_content = false,'
-echo '	use_texture_alpha = true,'
+echo '	use_texture_alpha = "clip",'
 echo '	drop = "",'
-echo '	post_effect_color = {a=255, r='$FogR', g='$FogG', b='$FogB'},'
+echo '	post_effect_color = {a=128, r='$FogR', g='$FogG', b='$FogB'},'
+
+if [ "$TransmitsLight" = 1 ]; then
+	echo '	light_source = 14,'
+	fi
+
 echo '	node_box = {'
 echo '		type = "fixed",'
 echo '		fixed = {'$CoordsString'},'
 echo '	},'
+
+if [ "$WalkSound" = 1 ]; then
+	echo '	sounds = default.node_sound_wood_defaults(),'
+	fi
+if [ "$WalkSound" = 2 ]; then
+	echo '	sounds = default.node_sound_gravel_defaults(),'
+	fi
+if [ "$WalkSound" = 3 ]; then
+	echo '	sounds = default.node_sound_leaves_defaults(),'
+	fi
+if [ "$WalkSound" = 4 ]; then
+	echo '	sounds = default.node_sound_stone_defaults(),'
+	fi
+if [ "$WalkSound" = 5 ]; then
+	echo '	sounds = default.node_sound_metal_defaults(),'
+	fi
+if [ "$WalkSound" = 6 ]; then
+	echo '	sounds = default.node_sound_glass_defaults(),'
+	fi
+if [ "$WalkSound" = 8 ]; then
+	echo '	sounds = default.node_sound_sand_defaults(),'
+	fi
+if [ "$WalkSound" = 9 ]; then
+	echo '	sounds = default.node_sound_snow_defaults(),'
+	fi
+
+if [ "$CollideType" = 5 ] || [ "$CollideType" = 6 ]; then
+	echo '	drowning = 1,'
+	echo '	liquidtype = "source",'
+	echo '	liquid_viscosity = 1,'
+	echo '	groups = {water=3, liquid=3, puts_out_fire=1},'
+	fi
 echo '})'
