b4ab2398639f82465b8cb06b0f2938c5181b4b76
TheCrafterDays <colbydray@mail.com>
2021-11-26 03:22:20 -0500

    Update customblocks_1_extractblockdefs.sh

--- scripts/customblocks_1_extractblockdefs.sh
+++ scripts/customblocks_1_extractblockdefs.sh
@@ -1,10 +1,6 @@
-echo ClassiCube2Minetest: Block: 1: Extracting world.json
-mkdir extracted
-cat $@ | gzip -d | ./nbt2json --big-endian > extracted/world.json
-echo ClassiCube2Minetest: Block: 1: Create Metadata.json
 mkdir extracted_custom
-cat extracted/world.json | jq '.nbt[] | .value[] | select(.name == "Metadata") ' > extracted_custom/Metadata.json
-echo ClassiCube2Minetest: Block: 1: Create BlockDefinitions.json
-cat extracted_custom/Metadata.json | jq '.value[] | select(.name == "CPE") | .value[] | select(.name == "BlockDefinitions") | .value[] ' > extracted_custom/BlockDefinitions.json
-echo ClassiCube2Minetest: Block: 1: Create BlockNames.json
-cat extracted_custom/BlockDefinitions.json | jq '.name' > extracted_custom/BlockNames.json 
+echo ClassiCube2Minetest: Block: Create BlockDefinitions.json
+cat extracted_custom/Metadata.json | jq -r '.CPE | .BlockDefinitions ' > extracted_custom/BlockDefinitions.json
+
+echo ClassiCube2Minetest: Block: Create BlockNames.json
+cat extracted_custom/Metadata.json | jq -r '.CPE | .BlockDefinitions | keys[] as $k | "\($k)"' > extracted_custom/BlockNames.json 
