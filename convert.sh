2851353d185bfa516918f9fd83d9894659c63472
TheCrafterDays <colbydray@mail.com>
2021-11-26 20:06:00 -0500

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,5 +1,14 @@
-scripts/customblocks_1_extractblockdefs.sh $@
+if [ $# -eq 0 ]
+then
+      echo "convert.sh packname cw_file"
+      exit
+fi
+
+rm -rf extracted_custom/*
+rm -rf extracted/*
+scripts/extractmetadata.sh ${@:2}
+scripts/customblocks_1_extractblockdefs.sh ${@:2}
 scripts/customblocks_2_blockseperatejson.sh
 scripts/customtexture_1_download.sh
-scripts/customtexture_2_rip.sh
+scripts/customtexture_2_rip.sh $1
 scripts/converttomtmod_1.sh
