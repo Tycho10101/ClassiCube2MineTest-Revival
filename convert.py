ee0d482c0e035cdf7944267c9992d60bd09ce29d
TheCrafterDays <colbydray@mail.com>
2022-07-04 18:52:32 -0400

    Update convert.py

--- convert.py
+++ convert.py
@@ -19,7 +19,6 @@ worldname = basenamewithext.split('.')[0]
 cc2mt.CCLoadMap(ClassicWorld)
 cc2mt.ConvertBlocks(MapName, worldname) #blocks_modname
 cc2mt.ConvertEnv(MapName + 'world', worldname) #blocks_worldname
-shutil.rmtree('./texture')
 cc2mt.ConvertWorld(MapName, worldname, 0, 0, 0, False) #using blocks_modname blocks
 CC_WorldSpawn = cc2mt.GetSpawnData()
 cc2mt.MT_MakePlayersFile(worldname, int(CC_WorldSpawn[0]['X']), int(CC_WorldSpawn[0]['Y']), int(CC_WorldSpawn[0]['Z']), CC_WorldSpawn[1])
